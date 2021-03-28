391271ec9a2e82efc397b89d4035651c4bda1392
Kian Siong <kian.siong@envolvedata.com>
2021-03-28 23:03:38 +0800

    Bug fix

--- section2_databases/queries.sql
+++ section2_databases/queries.sql
@@ -3,13 +3,13 @@
 select customer_name, sum(price) as total_spends
 from transactions
 group by customer_name
-order by total_spends desc
+order by total_spends desc;
 
 -- Top 3 car manufacturers by sales (quantity) and the sales number for the current month
 select manufacturers.name as manufacturer, count(*) as quantity_sold, sum(price) as total_sales
 from transactions
 inner join cars on transactions.car_id = cars.id
-inner join manufacturers on cars.manufacturer_id = manufacturer.id
+inner join manufacturers on cars.manufacturer_id = manufacturers.id
 where extract(MONTH from transactions.datetime) = extract(month from CURRENT_DATE)
 group by manufacturers.name
-order by manufacturers.name 
+order by manufacturers.name
